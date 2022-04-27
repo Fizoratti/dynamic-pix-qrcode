6ef9a93135965b680b8722a29494c9c7f20c807a
Robson Felipe Bittencourt <Robson_Bittencourt@Dellteam.com>
2022-04-27 19:01:44 -0300

    feat(qrcode, pix): generate a working pix qrcode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { QRCodeSVG } from 'qrcode.react';
+import { Payload } from 'node-pix';
 
 import './App.css';
 
 function App() {
+	const codigoPix = Payload.generate({
+		pixKey: '123e4567-e12b-12d1-a456-426655440000',
+		merchant: {
+			name: 'Fulano de Tal',
+			city: 'BRASILIA',
+		},
+	});
+
 	return (
 		<div className='App'>
-			<QRCodeSVG value='A' />
+			<p>{'Código PIX'}</p>
+			<p>{codigoPix.toString()}</p>
+			<QRCodeSVG value={codigoPix} />
 		</div>
 	);
 }
